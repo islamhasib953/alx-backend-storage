bf21c7bceead45cd2e6b9ce1a5c8706886966c9b
islamhasib953 <islamhasib953@example.com>
2024-08-22 22:35:32 +0300

    7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,15 +4,13 @@ DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
-
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE user_id = user_id;
-
+    DECLARE sum_score INT DEFAULT = 0;
+    DECLARE total_count INT DEFAULT = 0;
+    SELECT SUM(score) INTO sum_score FROM corrections WHERE corrections.user_id = user_id;
+    SELECT COUNT(*) INTO total_count FROM corrections WHERE corrections.user_id = user_id;
     UPDATE users
-    SET average_score = avg_score
-    WHERE id = user_id;
+        SET users.average_score = (sum_score / total_count) WHERE users.id = user_id;
+
 END $$
 
 DELIMITER;
