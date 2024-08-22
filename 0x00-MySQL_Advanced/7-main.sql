16d9a17ccbe4e2bf2e0fe3160871fe3a5ca82dc6
islamhasib953 <islamhasib953@example.com>
2024-08-22 22:23:27 +0300

    7-average_score.sql

---
+++ 0x00-MySQL_Advanced/7-main.sql
@@ -0,0 +1,19 @@
+-- Show and compute average score
+SELECT * FROM users;
+
+SELECT * FROM corrections;
+
+SELECT "--";
+
+CALL ComputeAverageScoreForUser (
+    (
+        SELECT id
+        FROM users
+        WHERE
+            name = "Jeanne"
+    )
+);
+
+SELECT "--";
+
+SELECT * FROM users;
