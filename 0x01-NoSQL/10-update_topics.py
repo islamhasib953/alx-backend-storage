5cdc083ab8c02072a8061967d10ddceca2abe8ee
islamhasib953 <islamhasib953@example.com>
2024-08-27 19:32:41 +0300

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" 10-update_topics """
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+    result = mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    return result
