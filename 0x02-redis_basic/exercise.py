b79d9a819d9527fa7798c0444db8ba4a35c822cf
islamhasib953 <islamhasib953@example.com>
2024-08-28 23:54:09 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 """ Task1 exercise """
 
 import redis, uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,3 +18,25 @@ class Cache:
         rand_num = str(uuid.uuid4())
         self._redis.set(rand_num, data)
         return rand_num
+
+    def get(
+        self, key: str, fn: Optional[callable] = None
+    ) -> Union[str, bytes, int, float]:
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """return value as a string"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """return value as a intger"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
